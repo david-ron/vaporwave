88ca12bbf2fcbf663a517666cbf2fadb7b39ec49
david-ron <ronci.david@gmail.com>
2019-08-10 15:39:04 -0400

    small changes shadowmap

---
+++ Source/ShadowMapDetails.cpp
@@ -0,0 +1,9 @@
+//
+//  ShadowMapDetails.cpp
+//  COMP371_Framework
+//
+//  Created by David Ronci on 2019-08-10.
+//  Copyright © 2019 Concordia. All rights reserved.
+//
+
+#include "ShadowMapDetails.hpp"
---
+++ Source/ShadowMapDetails.hpp
@@ -0,0 +1,14 @@
+//
+//  ShadowMapDetails.hpp
+//  COMP371_Framework
+//
+//  Created by David Ronci on 2019-08-10.
+//  Copyright © 2019 Concordia. All rights reserved.
+//
+
+#ifndef ShadowMapDetails_hpp
+#define ShadowMapDetails_hpp
+
+#include <stdio.h>
+
+#endif /* ShadowMapDetails_hpp */
